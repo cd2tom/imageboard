e42227ce1b9e244946ab2fe9a1105559bca1bb21
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2019-02-09 10:20:12 +0000

    fixes timestamps being a posix string

--- web/src/components/Thread/Meta.jsx
+++ web/src/components/Thread/Meta.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
-export default function Board({ post, handle }) {
+export default function Meta({ post, handle }) {
+  console.log(post);
   return (
     <div className="meta">
       {post.subject && <b className="subject">{post.subject}</b>}
       <b className="name">{post.name ? post.name : "Anon"}</b>
       <span>
-        {moment.unix(post.createdAt).format("MM/DD/YY(ddd) h:mm:ssa")}
+        {moment(Number(post.createdAt)).format("MM/DD/YY(ddd) h:mm:ssa")}
       </span>
       <span>No.{post.id}</span>
       {handle && <Link to={`/${handle}/${post.id}`}>[Reply]</Link>}
